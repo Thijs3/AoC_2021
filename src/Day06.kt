dcfc5d82d4a258b950d27660163e0a5d748028c6
Thijs Visser <thijsvisser@bol.com>
2021-12-06 15:09:46 +0100

    code golfing

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,29 +1,20 @@
 import java.math.BigInteger
 
 fun main() {
-
     val dp = MutableList(257) { MutableList(9) { BigInteger.ZERO } }
 
     for (i in 0..256) {
-        for (j in 8 downTo 0) {
-            if (i <= j) {
-                dp[i][j] = BigInteger.ONE
-            } else {
-                dp[i][j] = dp[i - j - 1][6] + dp[i - j - 1][8]
-            }
+        for (j in 0..8) {
+            if (i <= j) dp[i][j] = BigInteger.ONE else dp[i][j] = dp[i - j - 1][6] + dp[i - j - 1][8]
         }
     }
 
-    fun part1(input: List<Int>, days: Int): BigInteger =
-        input.sumOf { dp[days][it] }
-
-    fun part2(input: List<Int>, days: Int): BigInteger =
-        part1(input, days)
+    fun solve(input: List<Int>, days: Int): BigInteger = input.sumOf { dp[days][it] }
 
     val input = readIntsOneLine("Day06")
     val testInput = readIntsOneLine("Day06_test")
-    check(part1(testInput, 80) == BigInteger("5934"))
-    check(part2(testInput, 256) == BigInteger("26984457539"))
-    println(part1(input, 80))
-    println(part2(input, 256))
+    check(solve(testInput, 80) == BigInteger("5934"))
+    check(solve(testInput, 256) == BigInteger("26984457539"))
+    println(solve(input, 80))
+    println(solve(input, 256))
 }
