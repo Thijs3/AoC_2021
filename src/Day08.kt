188aabf757e2b6e7624035e20fdd5581d43723a7
Thijs Visser <thijsvisser@bol.com>
2021-12-08 15:35:39 +0100

    Day 8 cleanup

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,34 +1,35 @@
 fun main() {
     data class DisplayPuzzle(val examples: List<String>, val answer: List<String>)
 
-    fun readDisplayDigits(name: String) = readInput(name)
+    fun readDisplayPuzzles(name: String) = readInput(name)
         .map { line ->
             line.split("|")
         }
         .map { DisplayPuzzle(it[0].split(" "), it[1].split(" ")) }
 
-    fun intersectSize(string1: String, string2: String): Int {
-        val s1 = (string1).toCharArray().toMutableSet()
-        s1.retainAll(string2.toCharArray().toMutableSet())
-        return s1.size
+    fun numberOfCommonLetters(s1: String, examples: List<String>, len: Int): Int {
+        val s2 = examples.find { it.length == len }
+        val intersection = (s1).toCharArray().toMutableSet()
+        intersection.retainAll(s2!!.toCharArray().toMutableSet())
+        return intersection.size
     }
 
-    fun determineNumbers(puzzle: DisplayPuzzle): Int {
-        val result = puzzle.answer.map { ans ->
-            when (ans.length) {
+    fun solve(puzzle: DisplayPuzzle): Int {
+        val result = puzzle.answer.map { digit ->
+            when (digit.length) {
                 2 -> "1"
                 3 -> "7"
                 4 -> "4"
                 7 -> "8"
-                5 -> if (intersectSize(ans, puzzle.examples.find { it.length == 4 }!!) == 2) {
+                5 -> if (numberOfCommonLetters(digit, puzzle.examples, 4) == 2) {
                     "2"
-                } else if (intersectSize(ans, puzzle.examples.find { it.length == 2 }!!) == 2) {
+                } else if (numberOfCommonLetters(digit, puzzle.examples, 2) == 2) {
                     "3"
                 } else { "5" }
 
-                else -> if (intersectSize(ans, puzzle.examples.find { it.length == 2 }!!) == 1) {
+                else -> if (numberOfCommonLetters(digit, puzzle.examples, 2) == 1) {
                     "6"
-                } else if (intersectSize(ans, puzzle.examples.find { it.length == 4 }!!) == 4) {
+                } else if (numberOfCommonLetters(digit, puzzle.examples, 4) == 4) {
                     "9"
                 } else { "0" }
             }
@@ -40,10 +41,10 @@ fun main() {
         input.sumOf { it.answer.count { digit -> digit.length in listOf(2, 3, 4, 7) } }
 
     fun part2(input: List<DisplayPuzzle>): Int =
-        input.sumOf { puzzle -> determineNumbers(puzzle) }
+        input.sumOf { puzzle -> solve(puzzle) }
 
-    val input = readDisplayDigits("Day08")
-    val testInput = readDisplayDigits("Day08_test")
+    val input = readDisplayPuzzles("Day08")
+    val testInput = readDisplayPuzzles("Day08_test")
     check(part1(testInput) == 26)
     check(part2(testInput) == 61229)
     println(part1(input))
