798ae3ff1cf6802b2e2217fe1c38569b6dcca6a6
Thijs Visser <thijsvisser@bol.com>
2021-12-13 16:02:39 +0100

    we do a little refactoring

--- src/Day13.kt
+++ src/Day13.kt
@@ -10,31 +10,30 @@ fun main() {
 
     fun foldX(marked: List<Cell>, split: Int): List<Cell> {
         val output = mutableListOf<Cell>()
-        for (item in marked){
-            if (item.first >= split) { //maybe geq????
+        for (item in marked) {
+            if (item.first >= split) { // maybe geq????
                 output.add(Pair(split - (item.first - split), item.second))
-            }
-            else output.add(item)
+            } else output.add(item)
         }
         return output.toSet().toList()
     }
 
-    fun foldY(marked: List<Cell>, split: Int): List<Cell>{
+    fun foldY(marked: List<Cell>, split: Int): List<Cell> {
         val output = mutableListOf<Cell>()
         for (item in marked) {
             if (item.second >= split) {
                 output.add(Pair(item.first, split - (item.second - split)))
-            }
-            else output.add(item)
+            } else output.add(item)
         }
         return output.toSet().toList()
     }
 
-    fun prettyPrint(grid: List<List<String>>) {
+    fun prettyPrint(grid: List<List<String>>, title: String) {
+        println("$title GRID:")
         for (row in grid) {
             for (item in row) {
-                if (item == "#") print("⬜")
-                else print("⬛️️")
+                if (item == "#") print("⬜️")
+                else print("⬛️")
             }
             println()
         }
@@ -42,7 +41,8 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val marked = readMarked(input)
-        val newMarked = foldX(marked, 655)
+        val i = readInstructions(input)[0]
+        val newMarked = if (i.first == 'x') foldX(marked, i.second) else foldY(marked, i.second)
         return newMarked.size
     }
 
@@ -52,14 +52,17 @@ fun main() {
         val finalMarked = instructions.fold(marked) { acc, pair ->
             if (pair.first == 'x') foldX(acc, pair.second) else foldY(acc, pair.second)
         }
-        val grid = MutableList(6) { MutableList(39) { "." } }
+        val width = finalMarked.maxOf { it.first + 1 }
+        val height = finalMarked.maxOf {it.second + 1 }
+        val grid = MutableList(height) { MutableList(width) { "." } }
         for (mark in finalMarked) grid[mark.second][mark.first] = "#"
         return grid
     }
 
     val input = readInput("Day13")
     val testInput = readInput("Day13_test")
-    check(part1(testInput) == 18)
+    check(part1(testInput) == 17)
     println(part1(input))
-    prettyPrint(part2(input))
+    prettyPrint(part2(testInput), "TEST")
+    prettyPrint(part2(input), "RESULT")
 }
