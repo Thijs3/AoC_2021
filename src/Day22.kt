7c74e3dd20212f1be5ea0e0b702a5666eac742b6
Thijs Visser <thijsvisser@bol.com>
2021-12-22 15:52:03 +0100

    Day 22 final cleanup

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,38 +1,4 @@
 fun main() {
-    data class Instruction(val on: Boolean, val x: IntRange, val y: IntRange, val z: IntRange)
-    fun readInstructions(input: List<String>): List<Instruction> = input.map { line ->
-        Instruction(
-            line.split(" ")[0] == "on",
-            line.split(" ")[1].split(",")[0].split("=")[1].split("..")[0].toInt()..line.split(" ")[1].split(",")[0].split("=")[1].split("..")[1].toInt(),
-            line.split(" ")[1].split(",")[1].split("=")[1].split("..")[0].toInt()..line.split(" ")[1].split(",")[1].split("=")[1].split("..")[1].toInt(),
-            line.split(" ")[1].split(",")[2].split("=")[1].split("..")[0].toInt()..line.split(" ")[1].split(",")[2].split("=")[1].split("..")[1].toInt()
-        )
-    }
-
-    fun part1(input: List<String>): Int {
-        val cube = MutableList(101) { MutableList(101) { MutableList(101) { false } } }
-        val instructions = readInstructions(input)
-        for (i in instructions) {
-            for (x in i.x) {
-                val xi = x + 50
-                if (xi in 0..100) {
-                    for (y in i.y) {
-                        val yi = y + 50
-                        if (yi in 0..100) {
-                            for (z in i.z) {
-                                val zi = z + 50
-                                if (zi in 0..100) {
-                                    cube[xi][yi][zi] = i.on
-                                } else break
-                            }
-                        } else break
-                    }
-                } else break
-            }
-        }
-        return cube.flatten().flatten().count { it }
-    }
-
     data class Cube(val x: Pair<Long, Long>, val y: Pair<Long, Long>, val z: Pair<Long, Long>) {
         private val negatedCubes = mutableListOf<Cube>()
         fun volume(): Long =
@@ -55,11 +21,10 @@ fun main() {
             return Cube(Pair(newX[1], newX[2]), Pair(newY[1], newY[2]), Pair(newZ[1], newZ[2]))
         }
     }
+    data class Instruction(val on: Boolean, val cube: Cube)
 
-    data class Instruction2(val on: Boolean, val cube: Cube)
-
-    fun readInstructions2(input: List<String>): List<Instruction2> = input.map { line ->
-        Instruction2(
+    fun readInstructions(input: List<String>): List<Instruction> = input.map { line ->
+        Instruction(
             line.split(" ")[0] == "on",
             Cube(
                 Pair(line.split(" ")[1].split(",")[0].split("=")[1].split("..")[0].toLong(), line.split(" ")[1].split(",")[0].split("=")[1].split("..")[1].toLong()),
@@ -69,8 +34,32 @@ fun main() {
         )
     }
 
+    fun part1(input: List<String>): Int {
+        val space = MutableList(101) { MutableList(101) { MutableList(101) { false } } }
+        val instructions = readInstructions(input)
+        for (i in instructions) {
+            for (x in i.cube.x.first..i.cube.x.second) {
+                val xi = x + 50
+                if (xi in 0..100) {
+                    for (y in i.cube.y.first..i.cube.y.second) {
+                        val yi = y + 50
+                        if (yi in 0..100) {
+                            for (z in i.cube.z.first..i.cube.z.second) {
+                                val zi = z + 50
+                                if (zi in 0..100) {
+                                    space[xi.toInt()][yi.toInt()][zi.toInt()] = i.on
+                                } else break
+                            }
+                        } else break
+                    }
+                } else break
+            }
+        }
+        return space.flatten().flatten().count { it }
+    }
+
     fun part2(input: List<String>): Long {
-        val instructions = readInstructions2(input)
+        val instructions = readInstructions(input)
         val onCubes = mutableListOf<Cube>()
         instructions.forEach { instruction ->
             onCubes.forEach { cube ->
