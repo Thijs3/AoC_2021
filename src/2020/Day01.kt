a6db597ca73eca0b32d9aca2345407d25450582f
Thijs Visser <thijsvisser@bol.com>
2022-01-31 18:16:22 +0100

    Add day 1 2020

--- src/2020/Day01.kt
+++ src/2020/Day01.kt
@@ -1,21 +1,32 @@
+package `2020`
+
+import readInput
+
 fun main() {
-    fun part1(input: List<Int>): Int =
-        input.zipWithNext()
-            .count {
-                it.second > it.first
-            }
+    fun parseInput(input: List<String>) = input.map { it.toInt() }
+    fun part1(input: List<String>): Int {
+        val numbers = parseInput(input)
+        val required = numbers.map { 2020 - it }.toHashSet()
+        for (number in numbers) {
+            if (number in required) return number * (2020 - number)
+        }
+        return -1
+    }
 
-    fun part2(input: List<Int>): Int =
-        input.windowed(3)
-            .map { window -> window.sum() }
-            .zipWithNext()
-            .count {
-                it.second > it.first
-            }
+    fun part2(input: List<String>): Int {
+        val numbers = parseInput(input)
+        val pairs = numbers.mapIndexed { i1, n1 -> numbers.mapIndexed { i2, n2 -> if (i1 != i2) 2020 - (n1 + n2) to n1 else - 1 to -1} }.flatten()
+        val required = pairs.toMap()
+        for (number in numbers) {
+            if (required[number] != null) return number * required[number]!! * (2020 - required[number]!! - number)
+        }
+        return -1
+    }
 
-    val input = readInts("Day01")
-    val testInput = readInts("Day01_test")
-    check(part1(testInput) == 5)
+    val input = readInput("Day01", "2020")
+    val testInput = readInput("Day01_test", "2020")
+    check(part1(testInput) == 514579)
+    check(part2(testInput) == 241861950)
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String, year: String = "2021") = File("src/$year", "$name.txt").readLines()
 
 fun readInts(name: String) = readInput(name).map { it.toInt() }
 
